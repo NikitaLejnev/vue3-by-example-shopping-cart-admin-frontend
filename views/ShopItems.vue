e3da7db98fa5ced90f82aa7be41422a4703292fd
NikitaLejnev <catassasin331@gmail.com>
2022-01-28 11:52:35 +0300

    Added ShopItems with top bar items list in <template>

---
+++ views/ShopItems.vue
@@ -0,0 +1,16 @@
+<template>
+  <TopBar />
+  <h1>Shop Items</h1>
+  <button @click="showDialog = true">Add Item to Shop</button>
+  <div v-for="shopItem of shopItems"
+    :key="shopItem.shop_item_id">
+    <h2>{{ shopItem.name }}</h2>
+    <p>Description: {{ shopItem.description }}</p>
+    <p>Price: {{ shopItem.price }}</p>
+    <img :src="shopItem.image_url" :alt="shopItem.name" />
+    <br />
+    <button type="button" @click="deleteItem(shopItem)">
+      Delete Item from Shop
+    </button>
+  </div>
+</template>
