a483ece6e1055571696c76f1cd17e7cf090fd8af
NikitaLejnev <catassasin331@gmail.com>
2022-01-28 12:30:00 +0300

    Added <style> to ShopItems.vue

--- views/ShopItems.vue
+++ views/ShopItems.vue
@@ -151,3 +151,21 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+img {
+  width: 100px;
+}
+
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50;
+  transform: translate(-50%, -50%);
+  min-width: 400px;
+}
+
+.center input[type="text"] {
+  width: 100%;
+}
+</style>
